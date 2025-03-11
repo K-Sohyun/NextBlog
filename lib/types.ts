73fcb7b698597dcc5d195804029485380a81c203
DESKTOP-BSVGB20\sohyun <ksoh912@daonlink.com>
2025-03-11 13:27:38 +0900

    코드정렬

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,9 @@
 export interface Post {
-    id: string;
-    title: string;
-    date: string;
-    readingTime: string;
-    summary: string;
-    image: string;
-    contentHtml?: string; // Markdown 변환된 HTML 본문
-  }
-  
+  id: string;
+  title: string;
+  date: string;
+  readingTime: string;
+  summary: string;
+  image: string;
+  contentHtml?: string; // Markdown 변환된 HTML 본문
+}
